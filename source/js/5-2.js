7ea3b6a6e8c5bc65879c3c274a2cfde59a38714e
eason920 <eason920@gmail.com>
2021-03-14 20:36:17 +0800

    1100314-1

---
+++ source/js/5-2.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById("mycanvas")
+var ctx = canvas.getContext('2d');
+var blockWidth = 200;
+const PI = Math.PI;
+const PI2 = Math.PI*2;
+
+canvas.width = blockWidth * 3;
+canvas.height = blockWidth * 3;
